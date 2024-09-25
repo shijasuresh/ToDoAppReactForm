45c77e40fbeca6b12979a02d5bb039e6d8b591f7
shijasuresh <shija.suresh89@gmail.com>
2024-09-25 10:17:58 +1000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import TaskCard from './TaskCard';
+
+function App() {
+  let {register, handleSubmit, reset} = useForm();
+  let [tasks, setTasks] = useState([]);
+  
+  function addTaskHandler(data) {
+    setTasks([...tasks, data])
+    console.log(tasks)
+
+    reset();
+  }
+
+  return (
+    <div>
+      <div className="bg-blue-600 text-white text-center py-3 font-light">If you've got tasks, start managing today!</div>
+
+      <div className="w-[360px] mx-auto mt-16">
+          <h1 className="text-xl text-black font-bold">Manage your task <span className="text-neutral-500">@Shija</span></h1> 
+          <p className="text-neutral-600">Add your tasks and start organizing them quickly.</p>
+      </div>
+
+      <form action="" className='w-[360px] mx-auto mt-5' onSubmit={handleSubmit(addTaskHandler)}>
+        <div className="flex gap-2">
+            <input {...register('task')} className="w-[87%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="text" placeholder="e.g. Learn Javascript" />
+            <input {...register('dueDate')} className="w-[13%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="datetime-local" placeholder="e.g. Learn Javascript" />
+        </div>
+        <input type="submit" value="Add Task" className="bg-black rounded-lg text-white px-5 py-3 text-sm font-light" />
+      </form>
+
+      <div className="w-[360px] mx-auto mt-5">
+        {
+          tasks.length > 0 ? tasks.map((task,index)=> <TaskCard key={index} title={task.task} date={task.dueDate} />) : <h1>No Tasks</h1>
+        }
+     </div>      
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/TaskCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function TaskCard({title, date}) {
+  return (
+    <div>
+      <div className="border mb-3  rounded-xl p-3">    
+            <p>{title}</p>   
+            <p className="text-[14px] font-light  text-neutral-700">{date}</p>
+            <div className="flex gap-4 ">
+                <button id="task-completed" data-id="" className="text-neutral-400 hover:text-green-500 flex gap-3 font-light mt-2 text-sm items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check2-circle" viewBox="0 0 16 16">
+                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                  </svg>Mark as done</button>
+                <button id="delete-task" data-id="" className="text-neutral-400 hover:text-red-500 flex gap-3 font-light mt-2 text-sm items-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                  </svg> Delete</button>
+            </div>
+         </div>
+    </div>
+  )
+}
+
+export default TaskCard
