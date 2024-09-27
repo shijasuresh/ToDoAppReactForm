632b9b6c05822e1c7c35fb79e73db76fbe6ceb59
shijasuresh <shija.suresh89@gmail.com>
2024-09-27 13:07:32 +1000

    Firebase Code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
-import React, { useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import axios from 'axios';
 import { useForm } from 'react-hook-form'
-import TaskCard from './TaskCard';
+import TaskCard from './components/TaskCard';
+import Loader from './components/Loader';
+const firebaseUrl = 'https://frontend-cohort-db125-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
 function App() {
-  let {register, handleSubmit, reset} = useForm();
-  let [tasks, setTasks] = useState([]);
+  // let {register, handleSubmit, reset} = useForm();
+  let taskInput = useRef(null);
+  let [todos, setTodos] = useState([]);
+  let [formStatus, setFormStatus] = useState(false);
   
-  function addTaskHandler(data) {
-    setTasks([...tasks, data])
-    console.log(tasks)
+  // function addTaskHandler(data) {
+  //   setTasks([...tasks, data])
+  //   reset();
+  // }
 
-    reset();
+  function handleSubmit() {
+    setFormStatus(true);
+    let task = taskInput.current.value;
+    axios.post(`${firebaseUrl}todos.json`, {
+      title: task
+    }).then(()=> {
+      setFormStatus(false);
+      fetchTodos();
+    })
   }
+
+  function fetchTodos() {
+    axios.get(`${firebaseUrl}todos.json`).then(todos=>{
+      let tempTodos = [];
+      for(let key in todos.data) {
+        let todo= {
+          id: key,
+          ...todos.data[key]
+        }
+        tempTodos.push(todo);
+      }
+      setTodos(tempTodos);
+    })
+  }
+
+  function handleDelete(id) {
+    axios.delete(`${firebaseUrl}todos/${id}.json`).then(()=>{
+      fetchTodos();
+    })
+  }
+
+  useEffect(()=>{
+    fetchTodos()
+  }, [])
+
 
   return (
     <div>
@@ -22,17 +61,22 @@ function App() {
           <p className="text-neutral-600">Add your tasks and start organizing them quickly.</p>
       </div>
 
-      <form action="" className='w-[360px] mx-auto mt-5' onSubmit={handleSubmit(addTaskHandler)}>
+      {/* <form action="" className='w-[360px] mx-auto mt-5' onSubmit={handleSubmit(addTaskHandler)}> */}
+      <div className='w-[360px] mx-auto mt-5'>
         <div className="flex gap-2">
-            <input {...register('task')} className="w-[87%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="text" placeholder="e.g. Learn Javascript" />
-            <input {...register('dueDate')} className="w-[13%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="datetime-local" placeholder="e.g. Learn Javascript" />
+            <input ref={taskInput} className="w-[87%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="text" placeholder="e.g. Learn Javascript" />
+            {/* <input {...register('task')} className="w-[87%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="text" placeholder="e.g. Learn Javascript" /> */}
+            {/* <input {...register('dueDate')} className="w-[13%] p-3 rounded-lg border mb-2 focus:outline-none focus:ring-4 focus:ring-blue-100 focus:border-blue-600" type="datetime-local" placeholder="e.g. Learn Javascript" /> */}
         </div>
-        <input type="submit" value="Add Task" className="bg-black rounded-lg text-white px-5 py-3 text-sm font-light" />
-      </form>
+        <button onClick={handleSubmit} className="mt-2 bg-violet-200 py-3 px-5 text-violet-900 rounded-xl flex align-center gap-4">Create Todos{!formStatus ? "" : <Loader /> }</button>
+        </div>
+        {/* <input type="submit" value="Add Task" className="bg-black rounded-lg text-white px-5 py-3 text-sm font-light" /> */}
+      {/* </form> */}
 
       <div className="w-[360px] mx-auto mt-5">
         {
-          tasks.length > 0 ? tasks.map((task,index)=> <TaskCard key={index} title={task.task} date={task.dueDate} />) : <h1 className='text-center mt-5'>No Tasks</h1>
+          // tasks.length > 0 ? tasks.map((task,index)=> <TaskCard key={index} title={task.task} date={task.dueDate} />) : <h1 className='text-center mt-5'>No Tasks</h1>
+          todos.length > 0 ? todos.map(todo=> <TaskCard handleDelete={handleDelete} id={todo.id} title={todo.title} key={todo.id} />) : <h1 className='text-center mt-5'>No Tasks</h1>
         }
      </div>      
 
